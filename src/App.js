f9eb212926540fbc006b0271d2e8079f5ffa1eb5
SilentSweetness <chelsey.lovendahl@gmail.com>
2023-08-15 12:38:14 -0400

    added counters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Step />
     </div>
   );
 }
 
-export default App;
+function Step() {
+  const [stitches, setStitches] = useState(0);
+  const [row, setRow] = useState(0);
+  const [repeat, setRepeat] = useState(0);
+
+  return (
+    <div>
+      <div className="stitches">
+      <button onClick={() => setStitches((c) => c - 1)}>-</button>
+      <p>Stitches: {stitches}</p>
+      <button onClick={() => setStitches((c) => c + 1)}>+</button>
+      </div>
+      <div className="row">
+      <button onClick={() => setRow((c) => c - 1)}>-</button>
+      <p>Row: {row}</p>
+      <button onClick={() => setRow((c) => c + 1)}>+</button>
+      </div>
+      <div className="repeat">
+      <button onClick={() => setRepeat((c) => c - 1)}>-</button>
+      <p>Repeat: {repeat}</p>
+      <button onClick={() => setRepeat((c) => c + 1)}>+</button>
+      </div>
+    </div>
+  );
+}
